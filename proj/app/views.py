cf68866738da0986ddee851a74b164c5f6e377ab
Anton Vashenko <vashchenko-96@mail.ru>
2024-03-29 13:52:58 +0300

    add views for Bred and Dogs models and lists

--- proj/app/views.py
+++ proj/app/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Breed, Dog
+from .serializers import BreedSerializer, DogSerializer
+
+
+class BreedList(generics.ListCreateAPIView):
+    queryset = Breed.objects.all()
+    serializer_class = BreedSerializer
+
+
+class BreedDetails(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Breed.objects.all()
+    serializer_class = BreedSerializer
+
+
+class DogList(generics.ListCreateAPIView):
+    queryset = Dog.objects.all()
+    serializer_class = DogSerializer
+
+
+class DogDetails(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Dog.objects.all()
+    serializer_class = DogSerializer
